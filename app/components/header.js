130b531fc36382b71c8974d9571f58f9f45e1424
khoaguye <Knguyenn72@gmail.com>
2023-11-04 17:03:18 -0500

    header component

---
+++ app/components/header.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import toyotalogo from "public/toyotalogo.png";
+import { BiSolidUserCircle } from "react-icons/bi";
+function header() {
+  return (
+    <main className="flex justify-between ">
+      <div className="flex">
+        <img
+          src="/toyotalogo.png"
+          alt="Toyota Logo"
+          className="w-[150px] h-[70px] md:w-[200px] md:h-[100px]"
+        />
+        <h1 className="text-[1.5rem] md:text-[2rem] mt-6 md:mt-8 font-bold">
+          MoodTunes
+        </h1>
+      </div>
+
+      <div className="text-tyt_red mt-5 md:mt-8 text-[1rem] md:text-[2rem]">
+        <BiSolidUserCircle size={"40px"} />
+      </div>
+    </main>
+  );
+}
+
+export default header;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import Header from "./components/header";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
+    <main className="py-2 px-4">
+      <Header />
     </main>
   );
 }
