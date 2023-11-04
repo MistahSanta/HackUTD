3f7e23615b5588d16d2f7a92775e7713ab4d9899
MistahSanta <jonathanle1111@gmail.com>
2023-11-04 17:41:50 -0500

    playback

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
-import Image from 'next/image'
+
+import Playbar from "./components/Playbar"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        
-      </div>
+    <main className="flex min-h-screen flex-col p-24 w-full ">
+      
+      <footer className="w-full h-[10vh] flex absolute bottom-0 left-0 justify-center bg-black"> 
+        <Playbar />
+      </footer>
     </main>
   )
 }
