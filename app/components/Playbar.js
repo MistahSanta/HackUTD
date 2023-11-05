eb0ca183808813c2f519268f3b6557d3380d4762
khoaguye <Knguyenn72@gmail.com>
2023-11-04 21:07:45 -0500

    new pull

--- app/components/Playbar.js
+++ app/components/Playbar.js
@@ -1,30 +1,28 @@
 "use client";
-
-import {BsFillPlayCircleFill} from 'react-icons/bs'
-import {RiSkipLeftFill} from 'react-icons/ri'
-import {RiSkipRightFill} from 'react-icons/ri'
+import { BsFillPlayCircleFill } from "react-icons/bs";
+import { RiSkipLeftFill } from "react-icons/ri";
+import { RiSkipRightFill } from "react-icons/ri";
 
 export default function Playbar() {
-    return (
-        <main className='flex flex-col w-[40vw] bg-blue-500 justify-center items-center'>
-            <div className='flex relative w-[30vw] h-[20vh] lg:w-[12vw] lg:h-[10vh] justify-between items-center'>
-                <RiSkipLeftFill 
-                    size={50} 
-                    className='hover:cursor-pointer '
-                    onClick={() => {}} />
-                <BsFillPlayCircleFill 
-                    size={70} 
-                    className='hover:cursor-pointer'
-                    onClick={ () => {}}
-                    />
-                <RiSkipRightFill 
-                    size={50} 
-                    className='hover:cursor-pointer'
-                    onClick={() => {}} 
-                />
-
-            </div>
-        </main>
-    )
-
+  return (
+    <main className="flex flex-col w-[40vw] bg-blue-500 justify-center items-center">
+      <div className="flex relative w-[30vw] h-[20vh] lg:w-[12vw] lg:h-[10vh] justify-between items-center">
+        <RiSkipLeftFill
+          size={50}
+          className="hover:cursor-pointer "
+          onClick={() => {}}
+        />
+        <BsFillPlayCircleFill
+          size={70}
+          className="hover:cursor-pointer"
+          onClick={() => {}}
+        />
+        <RiSkipRightFill
+          size={50}
+          className="hover:cursor-pointer"
+          onClick={() => {}}
+        />
+      </div>
+    </main>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Header from "./components/header";
 import Playlist from "./components/playlist";
+import Playbar from "./components/playbar";
 export default function Home() {
   return (
     <main className=" h-[100vh] flex flex-col justify-center items-center">
@@ -8,9 +9,9 @@ export default function Home() {
       <div className="flex justify-center items-center flex-grow">
         <Playlist />
       </div>
-      <footer className="flex flex-col h-[12vh] w-full bottom-0 absolute left-0 justify-center items-center bg-green-500"> 
+      <footer className="flex flex-col h-[12vh] w-full bottom-0 absolute left-0 justify-center items-center bg-green-500">
         <Playbar />
       </footer>
     </main>
-  )
+  );
 }
